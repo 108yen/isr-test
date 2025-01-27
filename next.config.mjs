3ebfd84d39439a0609fecb511e1be73a590cb5c3
108yen <kazuking.1911@gmail.com>
2025-01-27 18:07:50 +0900

    chore: update experimental

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    dynamicIO:true,
+    useCache:true
   }
-};
+}
 
-export default nextConfig;
+export default nextConfig
